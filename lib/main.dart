e1f443ba1fa93c15660d2e2f3b26553344cb8e79
yusseflt <yus.toutenge@gmail.com>
2021-06-02 17:47:01 -0300

    Removendo arquivos de Api.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
-import 'package:module_app/repository/data_manager.dart';
 import 'package:module_app/repository/route_manager.dart';
 import 'package:weather/weather.dart';
 
@@ -18,7 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'OpenSans'),
-      builder: (context, child) => DataManager(child: child!),
       debugShowCheckedModeBanner: false,
       initialRoute: 'home',
       routes: RouteManager().routes,
